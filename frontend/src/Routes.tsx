40fefc67fe2ac2935aeb1a10aa0ef129f82fe886
aparecido nepomuceno <isacnepomuceno.np@gmail.com>
2021-11-05 14:52:22 -0300

    First request

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,6 +1,7 @@
 import Dashboard from "pages/Dashboard";
 import Home from "pages/Home";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {Routes as Switch} from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 
 const Routes = () => {
     return (
